aaafb9935f34745ca6f8679c079c592daa7aaae3
Ajay Shakya <aaajjy864@gmail.com>
2023-11-26 23:36:28 +0530

    Create type_of_datatype.js

---
+++ type_of_datatype.js
@@ -0,0 +1,9 @@
+// primitive
+// Ex number, string, boolean, null, undefined, igint
+
+
+// non primitive (Reference)  
+// Ex array, object, Function 
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
