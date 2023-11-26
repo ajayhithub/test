2b05168c735390621c1ca93182538c040a9460f0
Ajay Shakya <aaajjy864@gmail.com>
2023-11-26 14:56:40 +0530

    Create datatypeConvertion.js

---
+++ datatypeConvertion.js
@@ -0,0 +1,39 @@
+// let a = "";
+ 
+// console.log(a);
+// console.log(typeof a);
+
+
+// let num = Number(a);
+// console.log(num);
+// console.log(typeof num);
+
+// convert string to number  by Number()
+// "123" ===> 123
+// "123asf" ===> NaN
+// "taagsg" ===> NaN
+// true ===> 1
+// false ===> 0
+// null ===> 0
+// "" ===> 0
+
+// Boolean()
+// String()
+
+// ******** operation  ********
+let str1 = "ajay"
+let str2 = "   shakya"
+let str3 = str1+str2;
+console.log(str3);       // ajay   shakya
+console.log(str3+"aaaa");   // ajay   shakyaaaaa
+console.log("12" + 4 + 4); // 1244
+console.log(4 + 4 + "12" + 2+2 + "99"); //  8122299
+
+
+
+
+
+
+
+
+
