8c24b2d4a11cdfa17fe283f9693bfa8182e99e81
Ajay Shakya <aaajjy864@gmail.com>
2023-11-30 17:32:30 +0530

    Create forEach.js

---
+++ forEach.js
@@ -0,0 +1,20 @@
+const arr = ["js","c","c++","node","java","py"];
+
+arr.forEach( function (val){
+    console.log(val);
+})
+
+arr.forEach((val)=>{
+    console.log(val);
+})
+
+function fun(val)
+{
+    console.log(val);
+}
+arr.forEach(fun);
+
+// general syntex
+// arr.forEach((val,ind,arr)=>{
+
+// })
