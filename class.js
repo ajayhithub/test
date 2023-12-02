8d83d77873d5cb70bee02e9700bf487ea8e535a7
Ajay Shakya <aaajjy864@gmail.com>
2023-12-02 21:06:44 +0530

    Create class.js

---
+++ class.js
@@ -0,0 +1,25 @@
+
+class  user {
+
+    constructor(user,email,password){
+        this.user = user;
+        this.email =user;
+        this.password = password;
+    }
+
+    encryptpassword(){
+        return `${this.password}abc`
+    }
+     changeuser(){
+        return `${this.user.toUpperCase()}`
+     }
+}
+
+const chai = new user("chai",'chai@.com',"123");
+console.log(chai.encryptpassword());
+console.log(chai.changeuser());
+
+
+
+
+
