af94eb7bf7ede92c5c525f87b183e51ded1de42a
Ajay Shakya <aaajjy864@gmail.com>
2023-12-04 22:03:17 +0530

    project2

--- project/project1/project1.js
+++ project/project1/project1.js
@@ -7,9 +7,9 @@ const arr = Array.from(btn);  // html collection convert into array
 // console.log(arr);
 arr.forEach((val)=>{
         val.addEventListener('click',(e)=>{
-           console.log(e.target);
-           body.setAttribute('id',val.id)
-         //console.log(val.id);
+          // console.log(e.target);
+          body.setAttribute('id',val.id)
+          //console.log(val.id);
         })
 })
 
---
+++ project/project2/pro2.js
@@ -0,0 +1,44 @@
+const form = document.querySelector('form');
+const box1 = document.querySelector('.box1');
+let w = -1;
+let h = -1;
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    w = document.querySelector('#height').value;
+    h = document.querySelector('#weight').value;
+   // console.log(typeof w);
+    if(w === ''|| h === '')
+    {
+   // const div = document.createElement('div');
+    box1.innerHTML = "please fill the data";
+   // box1.appendChild(div);
+    }
+    else{
+    //const div = document.createElement('div');
+    const val = Math.round(10000*w/(h*h));
+    if(val<=18.4)
+    {
+        box1.innerHTML = `your BMI is ${val}  it is Underweight`;
+       // box1.appendChild(div);
+    }
+    else if(val<=24.9)
+    {
+        box1.innerHTML = `your BMI is ${val}  it is Normal`;
+       // box1.appendChild(div);
+    }
+    else if(val<=39.9)
+    {
+        box1.innerHTML = `your BMI is ${val}  it is Overweight`;
+       // box1.appendChild(div);
+    }
+    else
+    {
+        box1.innerHTML = `your BMI is ${val}  it is Obese`;
+       // box1.appendChild(div); 
+    }
+    }
+    
+    form.reset();
+})
+
+
